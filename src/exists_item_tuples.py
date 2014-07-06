5dc3eb0e56a3a52a079d3a2a01f60553fd39f42b
Andres Rodriguez <anrocox@gmail.com>
2014-07-06 17:00:45 -0500

    How to know if an element exists within a tuple in python?

---
+++ src/exists_item_tuples.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 3, 2014
+
+@author: anroco
+
+How to know if an element exists within a tuple in python?
+
+¿como saber si un elemento existe dentro de una tupla python?
+'''
+
+#create a tuple
+tupla = ('a', 'b', 'c', 'd', 'e')
+print (tupla)
+
+#use the in statement
+print('c' in tupla)
+print(2 in tupla)
